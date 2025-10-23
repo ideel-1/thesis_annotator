95e6658542fc4b5ec7ea6f34a69c04cbc9fae486
Ideel-1 <rado.lamac@gmail.com>
2025-10-23 15:58:09 +0300

    update paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  basePath: "/thesis/annotate",
 };
 
 export default nextConfig;
