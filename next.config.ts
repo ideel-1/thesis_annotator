20040e7dcf64f7cc39dc8e64dbc5e78c55f0f15e
Ideel-1 <rado.lamac@gmail.com>
2025-10-23 17:02:16 +0300

    updated pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true, // good for Pages hosting
+  reactStrictMode: true,
 };
 
 export default nextConfig;
